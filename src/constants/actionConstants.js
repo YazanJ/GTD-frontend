7830b044dc0d35bc4e038462d77fde66c1656f0a
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-03 16:19:26 +0000

    create constants folder

---
+++ src/constants/actionConstants.js
@@ -0,0 +1,16 @@
+export const states = ["inbox", "next", "wait", "sched", "smday"];
+export const timeOptions = [
+    "5 minutes",
+    "10 minutes",
+    "15 minutes",
+    "30 minutes",
+    "45 minutes",
+    "1 hour",
+    "2 hours",
+    "3 hours",
+    "4 hours",
+    "5 hours",
+    "6 hours",
+    "oh boy!",
+    "none",
+];
