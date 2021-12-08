c6f8bed3624704946c9e4baf2f17b8518389b263
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-08 13:08:24 +0000

    clean code

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,14 +1,37 @@
-import React, { useEffect, useState } from "react";
-import { getActions } from "../api";
+import React, { useState, useEffect } from "react";
+import ActionContainer from "../components/ActionContainer";
+import { useParams } from "react-router-dom";
+import { states } from "../constants/actionConstants";
+import { getActions, getStateActions, getFocusedActions } from "../api";
 
-const ActionScreen = () => {
+const MainScreen = () => {
+    // const [isFocus, setIsFocus] = useState(false)
+    // const [isActions, setIsActions] = useState(false)
+    // const [isProject]
     const [actions, setActions] = useState([]);
+    const { state } = useParams();
 
     useEffect(() => {
-        getActions().then((data) => setActions(data));
-    }, [actions]);
+        if (states.includes(state)) {
+            getStateActions(state)
+                .then((data) => {
+                    setActions(data);
+                })
+                .catch((error) => console.log(error));
+        } else if (state === "focused") {
+            getFocusedActions()
+                .then((data) => {
+                    setActions(data);
+                })
+                .catch((error) => console.log(error));
+        }
+    }, [state]);
 
-    return <div>MainScreen</div>;
+    return (
+        <div>
+            <ActionContainer actions={actions} />
+        </div>
+    );
 };
 
-export default ActionScreen;
+export default MainScreen;
