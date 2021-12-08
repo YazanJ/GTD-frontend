90f51d52318311c049f1c8cfa188add6f43ada22
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-08 12:58:37 +0000

    add new routes to fetch state and focsued
    git add src/api.js

--- src/api.js
+++ src/api.js
@@ -10,3 +10,25 @@ export const getActions = async () => {
         return error;
     }
 };
+
+export const getStateActions = async (state) => {
+    try {
+        const { data } = await axios.get(
+            `http://127.0.0.1:3000/api/users/actions/${state}/`
+        );
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const getFocusedActions = async () => {
+    try {
+        const { data } = await axios.get(
+            "http://127.0.0.1:3000/api/users/actions/focused/"
+        );
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
