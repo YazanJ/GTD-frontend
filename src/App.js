e9d8884dc03721e6da16a8237662da347c0a98a0
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-08 12:57:13 +0000

    initial app.js version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainScreen from "./screens/MainScreen";
+import { Row, Col } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Header />
+            <Row>
+                <Col md={3}>
+                    <Sidebar className="min-vh-100"></Sidebar>
+                </Col>
+                <Col md={9}>
+                    <main className="py-1">
+                        <Container>
+                            <Routes>
+                                <Route path="/" element={<MainScreen />} />
+                                <Route
+                                    path="/actions/:state/"
+                                    element={<MainScreen />}
+                                />
+                            </Routes>
+                        </Container>
+                    </main>
+                </Col>
+            </Row>
+        </Router>
+    );
+};
 
 export default App;
