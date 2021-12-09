83f12c674a9d02bd7ad5135d810e30a83afc42e9
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-09 12:29:18 +0000

    remove redundant api funcs to backend endpoints

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// get all actions from db
 export const getActions = async () => {
     try {
         const { data } = await axios.get(
@@ -7,28 +8,6 @@ export const getActions = async () => {
         );
         return data;
     } catch (error) {
-        return error;
-    }
-};
-
-export const getStateActions = async (state) => {
-    try {
-        const { data } = await axios.get(
-            `http://127.0.0.1:3000/api/users/actions/${state}/`
-        );
-        return data;
-    } catch (error) {
-        return error;
-    }
-};
-
-export const getFocusedActions = async () => {
-    try {
-        const { data } = await axios.get(
-            "http://127.0.0.1:3000/api/users/actions/focused/"
-        );
-        return data;
-    } catch (error) {
-        return error;
+        return error.message;
     }
 };
