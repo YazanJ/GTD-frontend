7acae1de02ea9e0d4cd8a8452e98e8d7360a9847
Yazan Judeh <yazanjudeh@outlook.com>
2021-12-09 15:42:36 +0000

    add getProjects func

--- src/api.js
+++ src/api.js
@@ -1,11 +1,20 @@
 import axios from "axios";
+const HOSTNAME = "http://127.0.0.1:3000";
 
 // get all actions from db
 export const getActions = async () => {
     try {
-        const { data } = await axios.get(
-            "http://127.0.0.1:3000/api/users/actions/"
-        );
+        const { data } = await axios.get(`${HOSTNAME}/api/users/actions/`);
+        return data;
+    } catch (error) {
+        return error.message;
+    }
+};
+
+// get all projects from db
+export const getProjects = async () => {
+    try {
+        const { data } = await axios.get(`${HOSTNAME}/api/users/projects/`);
         return data;
     } catch (error) {
         return error.message;
